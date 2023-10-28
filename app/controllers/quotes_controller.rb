f5552bd8d05f7bc24cee1db957b941cb9984f59e
Alex Mikitsik <mikitsik@gmail.com>
2023-10-28 23:10:03 +0300

    Add controller

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class QuotesController < RailsFromScratch::Controller
+  def a_quote
+    "There is nothing either good or bad " +
+      "but thinking makes it so."
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 require "rails_from_scratch"
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+require "quotes_controller"
+
 module SomeBestQuotesEver
-  class Application < RailsFromScratch::Application
-  end
+  class Application < RailsFromScratch::Application; end
 end
