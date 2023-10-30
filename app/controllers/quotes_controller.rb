153cd3b24fdbf01984a1e56a2aef8a0a9f08e75a
Alex Mikitsik <mikitsik@gmail.com>
2023-10-30 07:11:35 +0300

    Add rack env output

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,8 +1,17 @@
-# frozen_string_literal: true
+
 
 class QuotesController < RailsFromScratch::Controller
   def a_quote
     "There is nothing either good or bad " +
-      "but thinking makes it so."
+      "but thinking makes it so." +
+      "\n<pre>\n#{format_request(env)}\n</pre>"
+  end
+
+  private
+
+  def format_request(data)
+    formatted_string = "{\n"
+    data.each { |key, value| formatted_string << " '#{key}' => '#{value}',\n" }
+    formatted_string << "\n}"
   end
 end
